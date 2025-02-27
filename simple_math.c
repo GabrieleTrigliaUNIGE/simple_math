ba38073a1805d388434a9e0ff0542879d7861939
Gabriele Triglia <gabrieletriglia04@gmail.com>
2025-02-27 13:27:07 +0100

    first version of simple math - no test

---
+++ simple_math.c
@@ -0,0 +1,15 @@
+/**
+ * \file simple_math.h
+ * \brief a toy example of math library
+ * \author Gabriele Triglia
+ */
+#include "simple_math.h"
+
+/**
+ * \param a primo numero
+ * \param b secondo numero
+ * \return media 
+ */
+float mean(int a, int b){
+    return (a + b)/2;
+}
---
+++ simple_math.h
@@ -0,0 +1,12 @@
+/**
+* \file simple_math.h
+* \brief a toy example of math library
+* \author Gabriele Triglia
+*/
+
+#ifndef _SIMPLEMATH_
+    #define _SIMPLEMATH_
+    float mean (int a, int b);
+#endif 
+
+
